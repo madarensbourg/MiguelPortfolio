295f03a5763090d1d8c87584d5da2b38d8c1d4b7
madarensbourg <madarens@ucsd.edu>
2023-02-06 15:18:35 -0800

    new portfolio

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+		<div className='App'>
+			<h1>Hello World</h1>
+			<Routes>
+			
+			</Routes>
+		</div>
+	);
+}
+
+export default App;
